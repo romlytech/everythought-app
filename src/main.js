b0fb9f6c344f6bd2af77238cc92420ebc0713975
filtersource <65119459+filtersource@users.noreply.github.com>
2021-10-09 23:37:31 -0400

    test router

--- src/main.js
+++ src/main.js
@@ -29,8 +29,8 @@ router.beforeEach(async (to, from, next) => {
     return next({ path: "/login" });
   } else if (to.meta.requiresAuth && !store.user) {
     return next({ path: "/login" });
-  } else if (to.path == "/" && store.user) {
-    return next({ path: "/dashboard" });
+    // } else if (to.path == "/" && store.user) {
+    //   return next({ path: "/dashboard" });
   } else if (to.path == "/login" && store.user) {
     return next({ path: "/dashboard" });
   }
