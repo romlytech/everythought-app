193630783140603ffae6dfbb2e85c713524931e1
filtersource <65119459+filtersource@users.noreply.github.com>
2021-10-11 00:11:51 -0400

    fix routes (an attempt)

--- src/main.js
+++ src/main.js
@@ -23,10 +23,10 @@ router.beforeEach(async (to, from, next) => {
     store.alert_msg = "Sorry, there was a problem. Please try again.";
     store.error = true;
     return next({ path: "/" });
+  } else if (to.fullPath.indexOf("type=recovery") != -1) {
+    return next({ path: "/reset" });
   } else if (to.hash.includes("access_token")) {
     return next({ path: "/dashboard" });
-  } else if (to.fullPath.indexOf("type=recovery") != -1) {
-    next({ path: "/reset" });
   } else if (to.hash.includes("message")) {
     store.alert_msg = to.hash.split("#message=")[1].replaceAll("+", " ");
     store.error = false;
